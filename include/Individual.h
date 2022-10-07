20011e6674e2fd9294ca43f556afe85a56cd81e2
vdaghan <36645163+vdaghan@users.noreply.github.com>
2022-10-07 23:58:37 +0300

    IndividualIdentifier comparison functions

--- include/Individual.h
+++ include/Individual.h
@@ -10,10 +10,25 @@ namespace DEvA {
 	struct IndividualIdentifier {
 		std::size_t generation;
 		std::size_t identifier;
+		friend bool operator<(IndividualIdentifier const & l, IndividualIdentifier const & r) {
+			return std::tie(l.generation, l.identifier) < std::tie(r.generation, r.identifier);
+		}
+		friend bool operator>(IndividualIdentifier const & lhs, IndividualIdentifier const & rhs) {
+			return rhs < lhs;
+		}
+		friend bool operator<=(IndividualIdentifier const & lhs, IndividualIdentifier const & rhs) {
+			return !(lhs > rhs);
+		}
+		friend bool operator>=(IndividualIdentifier const & lhs, IndividualIdentifier const & rhs) {
+			return !(lhs < rhs);
+		}
+		friend bool operator==(IndividualIdentifier const & lhs, IndividualIdentifier const & rhs) {
+			return lhs.generation == rhs.generation and lhs.identifier == rhs.identifier;
+		}
+		friend bool operator!=(IndividualIdentifier const & lhs, IndividualIdentifier const & rhs) {
+			return !(lhs == rhs);
+		}
 	};
-	bool operator==(IndividualIdentifier const & lhs, IndividualIdentifier const & rhs) {
-		return lhs.generation == rhs.generation and lhs.identifier == rhs.identifier;
-	}
 	using IndividualIdentifiers = std::vector<IndividualIdentifier>;
 
 	template <typename Types, typename VP>
